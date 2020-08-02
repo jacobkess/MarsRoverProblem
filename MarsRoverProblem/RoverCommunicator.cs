a74338c62c9815b9f50dc4032d24492274f29f8f
Jacob Kessler <jacob.kessler@case.edu>
2020-08-02 13:09:46 -0400

    Initial Commit

---
+++ MarsRoverProblem/RoverCommunicator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsRoverProblem
+{
+    class RoverCommunicator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mars!");
+        }
+    }
+}
---
+++ MarsRoverTests/RoverCommunicatorTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRoverTests
+{
+    [TestClass]
+    public class RoverCommunicatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
